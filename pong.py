74cc935525e6414631927478bd0a30c795b9f1ab
Andrew Smith <atsmith19@comcast.net>
2025-05-17 15:30:14 -0700

    Adding pre commit checks for python syntax

--- pong.py
+++ pong.py
@@ -7,7 +7,9 @@ from game import GameState
 WHITE = sdl2.ext.Color(255, 255, 255)
 RED = sdl2.ext.Color(255, 0, 0)
 
+
 class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
+
     def __init__(self, window):
         super(SoftwareRenderer, self).__init__(window)
 
@@ -17,26 +19,35 @@ class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
 
 
 class Player(sdl2.ext.Entity):
+
     def __init__(self, world, sprite, posx=0, posy=0):
         self.sprite = sprite
         self.sprite.position = posx, posy
+
 
 class Ball(sdl2.ext.Entity):
+
     def __init__(self, world, sprite, posx=0, posy=0):
         self.sprite = sprite
         self.sprite.position = posx, posy
 
+
 class GameGUI:
+
     def __init__(self, world, game):
         factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-        sp_paddle = factory.from_color(WHITE, size=(1, int(game.paddle_h*200)))
-        self.paddle = Player(world, sp_paddle, 199, int(game.paddle_y*200))
-        sp_ball = factory.from_color(RED, size=(2,2))
-        self.ball = Ball(world, sp_ball, int(game.ball_x*200), int(game.ball_y*200))
+        sp_paddle = factory.from_color(WHITE,
+                                       size=(1, int(game.paddle_h * 200)))
+        self.paddle = Player(world, sp_paddle, 199, int(game.paddle_y * 200))
+        sp_ball = factory.from_color(RED, size=(2, 2))
+        self.ball = Ball(world, sp_ball, int(game.ball_x * 200),
+                         int(game.ball_y * 200))
 
     def update(self, game):
-        self.paddle.sprite.position = (199, int(game.paddle_y*200))
-        self.ball.sprite.position = (int(game.ball_x*200), int(game.ball_y*200))
+        self.paddle.sprite.position = (199, int(game.paddle_y * 200))
+        self.ball.sprite.position = (int(game.ball_x * 200),
+                                     int(game.ball_y * 200))
+
 
 def run():
     running = True
@@ -49,7 +60,7 @@ def run():
 
     game = GameState()
     game.paddle_h = 1.0
-    game_gui = GameGUI(world,game)
+    game_gui = GameGUI(world, game)
 
     while running:
         events = sdl2.ext.get_events()
@@ -57,7 +68,7 @@ def run():
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
-        game.update(1) # Do Nothing
+        game.update(1)  # Do Nothing
         game_gui.update(game)
         print(game.get_context())
         if game.over:
@@ -66,6 +77,7 @@ def run():
             #break
         sdl2.SDL_Delay(10)
         world.process()
+
 
 if __name__ == "__main__":
     sys.exit(run())
